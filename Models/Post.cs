2a0edb7133349439a6ad74140c4e595ae12ecc5c
pullaposki <pullaposki@windowslive.com>
2023-11-21 13:32:01 +0200

    Add project files.

---
+++ Models/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorBloggingApp.Models
+{
+	public class Post
+	{
+		public string Title { get; set; }
+		public string Body { get; set; }
+		public string AuthorName { get; set; }
+	}
+}
